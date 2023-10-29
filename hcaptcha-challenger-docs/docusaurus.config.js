456f3a79f667643a9ae5eeee4ba685426fca3555
QIN2DIM <62018067+QIN2DIM@users.noreply.github.com>
2023-10-30 04:01:14 +0800

    ffe

---
+++ hcaptcha-challenger-docs/docusaurus.config.js
@@ -0,0 +1,107 @@
+// @ts-check
+// Note: type annotations allow type checking and IDEs autocompletion
+
+const lightCodeTheme = require('prism-react-renderer/themes/github');
+const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: 'CaptchaAgent',
+  tagline: '🥂 Gracefully face hCaptcha Challenge with MoE(ONNX) embedded solution.',
+  favicon: 'img/favicon2.png',
+
+  // Set the production url of your site here
+  url: 'https://github.com',
+  // Set the /<baseUrl>/ pathname under which your site is served
+  // For GitHub pages deployment, it is often '/<projectName>/'
+  baseUrl: '/CaptchaAgent/',
+
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  organizationName: 'CaptchaAgent', // Usually your GitHub org/user name.
+  projectName: 'hcaptcha-challenger-docs', // Usually your repo name.
+
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+
+  // Even if you don't use internalization, you can use this field to set useful
+  // metadata like html lang. For example, if your site is Chinese, you may want
+  // to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en', 'zh-Hans'],
+  },
+
+  presets: [
+    [
+      'classic',
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          sidebarPath: require.resolve('./sidebars.js'),
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          editUrl:
+            'https://github.com/CaptchaAgent/hcaptcha-challenger-docs/tree/main/hcaptcha-challenger-docs/',
+        },
+        blog: {
+          showReadingTime: true,
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          editUrl:
+            'https://github.com/CaptchaAgent/hcaptcha-challenger-docs/tree/main/hcaptcha-challenger-docs/',
+        },
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      }),
+    ],
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+
+      // Replace with your project's social card
+      image: 'img/CaptchaAgent-social-card.jpg',
+      navbar: {
+        title: 'CaptchaAgent',
+        logo: {
+          alt: 'CaptchaAgent',
+          src: 'img/logo.png',
+        },
+        items: [
+          {
+            type: 'docSidebar',
+            sidebarId: 'tutorialSidebar',
+            position: 'left',
+            label: 'Docs',
+          },
+          // https://docusaurus.io/zh-CN/docs/api/plugins/@docusaurus/plugin-content-blog#markdown-front-matter
+          { to: '/blog', label: 'Blog', position: 'left' },
+          {
+            type: "localeDropdown",
+            position: "right"
+          },
+          {
+            href: 'https://github.com/QIN2DIM/hcaptcha-challenger',
+            label: 'GitHub',
+            position: 'right',
+          }
+        ],
+      },
+      footer: {
+        style: 'dark',
+        links: [
+
+        ],
+        copyright: `Copyright © ${new Date().getFullYear()} CaptchaAgent. All right.`,
+      },
+      prism: {
+        theme: lightCodeTheme,
+        darkTheme: darkCodeTheme,
+      },
+    }),
+};
+
+module.exports = config;
